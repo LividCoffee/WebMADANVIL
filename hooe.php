70ed7da8c431c32fa66c1933561fb91ec8af3406
lividcoffee <lividgame@gmail.com>
2018-09-24 21:30:46 +0300

    #

--- hooe.php
+++ hooe.php
@@ -6,8 +6,3 @@
     <h1>Success!  The rabotyag.ga virtual host is working!</h1>
   </body>
 </html>
-
-<?php
-	$params = explode(",", $argv[1]);
-	echo $params[0] ." - " . $params[1] . " - " . $params[2];
-?>
