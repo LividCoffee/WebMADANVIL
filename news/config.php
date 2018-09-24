f2385164e26b8e43c6f2f201f3626c83764fcf09
lividcoffee <lividgame@gmail.com>
2018-09-25 00:53:33 +0300

    Add NEWS

---
+++ news/config.php
@@ -0,0 +1,44 @@
+<?php return array (
+  'application' => 
+  array (
+    'debug' => false,
+  ),
+  'database' => 
+  array (
+    'default' => 'mysql',
+    'connections' => 
+    array (
+      'mysql' => 
+      array (
+        'host' => 'localhost',
+        'user' => 'madanvil',
+        'password' => '13141974zxCV',
+        'dbname' => 'madanvil',
+        'prefix' => 'pk_',
+      ),
+    ),
+  ),
+  'system' => 
+  array (
+    'secret' => 'j4bEVp7qiadrF/B3TNVvX/DYeb5lTxms2LJt/EKjTZW0gQclADhZEt1z690rDjAn',
+  ),
+  'system/cache' => 
+  array (
+    'caches' => 
+    array (
+      'cache' => 
+      array (
+        'storage' => 'auto',
+      ),
+    ),
+    'nocache' => false,
+  ),
+  'system/finder' => 
+  array (
+    'storage' => '/storage',
+  ),
+  'debug' => 
+  array (
+    'enabled' => false,
+  ),
+);
