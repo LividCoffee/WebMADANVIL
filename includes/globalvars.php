d3460ed208bb76e74d4f1dfac9f82b7660e482e2
lividcoffee <lividgame@gmail.com>
2018-09-24 21:26:45 +0300

    Add includes

---
+++ includes/globalvars.php
@@ -0,0 +1,16 @@
+<?php
+
+$GLOBALS['cookie_welcome'] = 'cookie_welcome';
+$GLOBALS['cookie_user'] = 'cookie_user';
+
+$GLOBALS['dbname'] = 'madanvil';
+$GLOBALS['dbhost'] = 'localhost';
+$GLOBALS['dbuser'] = 'madanvil';
+$GLOBALS['dbpassword'] = '13141974zxCV';
+
+//links
+$domain = 'http://rabotyag.ga/';
+$GLOBALS['linkforverify'] = $domain.'verify-account.php';
+$GLOBALS['linkforpassword'] = $domain.'reset-password-reset.php';
+$GLOBALS['linkforchangepassword'] = $domain.'reset-password.php';
+$GLOBALS['linkforitwasntme'] = $domain.'it-was-not-me.php';
