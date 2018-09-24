270f36e6c41a6c0fe3415088cdd511292fbdfbd2
lividcoffee <lividgame@gmail.com>
2018-09-24 22:35:03 +0300

    UPD

--- checkauth.php
+++ checkauth.php
@@ -2,11 +2,12 @@
 if(!defined("MenuSctipt")){ header('Location: appname.php'); }
 if(!isLoggedIn($db)): ?>
 <!-- Редирект если не зарегистрирован / авторизован -->
+
 <?php else:?>
 <?php echo $_SESSION['email']; ?>
-<!-- Редирект если зарегистрирован / авторизован -->
-<a href="/logout.php">Выйти</a>
 
+<!-- Редирект если зарегистрирован / авторизован -->
 <?php
- endif;
+header('Location: news.php');
+endif;
 ?>
